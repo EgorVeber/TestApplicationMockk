4c9a0d1c1bfe16606c02161287d5394f3c38e0a4
JamesOhara <egor.veber.02.07.95@gmail.com>
2023-01-31 23:22:37 +0300

    ExampleTest

---
+++ app/src/main/java/ru/gb/veber/testapplicationmockk/util/Consumer.kt
@@ -0,0 +1,13 @@
+package ru.gb.veber.testapplicationmockk.util
+
+/**
+ * Resource consumer.
+ * Usage example:
+ * ```
+ *     val consumer: Consumer<String> = { string ->
+ *         println(string)
+ *     }
+ *     resourceManager.consumeResource(consumer)
+ * ```
+ */
+typealias Consumer<R> = (R) -> Unit
--- app/src/test/java/ru/gb/veber/testapplicationmockk/ExampleUnitTest.kt
+++ app/src/test/java/ru/gb/veber/testapplicationmockk/ExampleUnitTest.kt
@@ -1,8 +1,19 @@
 package ru.gb.veber.testapplicationmockk
 
+import android.util.Log
+import io.mockk.*
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.junit4.MockKRule
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Rule
+import ru.gb.veber.testapplicationmockk.util.ErrorHandler
+import ru.gb.veber.testapplicationmockk.util.ResourceManager
+import java.util.concurrent.Executor
+import kotlin.random.Random
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +21,87 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    interface Bar {
+        fun print(n: Int): Boolean
+        fun foo()
+        fun getInt(): Int
+        fun getBoolean(): Boolean
+    }
+
+    @RelaxedMockK
+    lateinit var bar: Bar
+
+    @get:Rule
+    val rule = MockKRule(this)
+
+    @MockK
+    lateinit var testExecutor: Executor
+
+    @MockK
+    lateinit var testErrorHandler: ErrorHandler<String>
+
+    @InjectMockKs
+    lateinit var resourceManager: ResourceManager<String>
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun test() {
+        val testExecutor = mockk<Executor>(relaxed = true)
+        val testErrorHandler = mockk<ErrorHandler<String>>()
+        val resourceManager = ResourceManager(testExecutor, testErrorHandler)
+        val resourceManagerSpy = spyk(resourceManager)
+        resourceManagerSpy.destroy()
+        verify { resourceManagerSpy.destroy() }
+    }
+
+    @Test
+    fun test2() {
+        every { resourceManager.destroy() } just runs
+        resourceManager.destroy()
+        verify { resourceManager.destroy() }
+    }
+
+    @Test
+    fun test3() {
+
+        every { bar.foo() } answers { println("foo() method has been called") }
+        //every { bar.foo() } just runs
+
+        every { bar.getBoolean() } answers {
+            Random.nextBoolean()
+        }
+        // every { bar.getBoolean() } returns true
+
+        every { bar.getInt() } returns 42
+        //every { bar.getInt() } returns 42 andThen 7 andThen 22
+
+
+//        every { testExecutor.execute(any()) } answers {
+//            firstArg<Runnable>().run()
+//        }
+        bar.foo()
+        println("Bool:{${bar.getBoolean()}}")
+        println("Int:{${bar.getInt()}}")
+    }
+
+    @Test
+    fun test4() {
+        every { bar.print(less(0)) } returns false // меньше нуля то false
+
+        every { bar.print(more(10)) } answers {
+            println(firstArg<Int>())
+            true
+        }
+        println(bar.print(2))
+        bar.print(11)
+
+
+        every { bar.print(-1) } returns  true
+        every { bar.print(eq(-1)) } returns  true
+    }
+
+    @Test
+    fun test5() {
+
     }
 }
